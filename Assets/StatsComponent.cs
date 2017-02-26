ac80578ad7a74c03002c56672079be2456db17fb
Jason Mitchell <foobardog@hotmail.com>
2017-02-25 17:22:24 -0800

    Added Component to randomly walk the story and report on variable stats
    I just was having this question too often while writing.

---
+++ Assets/StatsComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StatsComponent : MonoBehaviour
+{
+    public TextAsset storyAsset;
+    public string[] variables;
+    public int trials = 100;
+    public string stoppingPoint = null;
+
+    private void Start()
+    {
+        foreach (string variable in variables)
+        {
+            var stats = StoryStats.CalculateVariableStats(storyAsset.text, variable, trials, stoppingPoint);
+            Debug.LogFormat("{0} -- Min: {1} Max: {2} Mean: {3} Median: {4} Modes: {5} Std. Dev: {6}",
+                variable,
+                stats.Minimum,
+                stats.Maximum,
+                stats.Mean,
+                stats.Median,
+                string.Join(", ", stats.Modes.Select(m => m.ToString()).ToArray()),
+                stats.StandardDeviation);
+        }
+    }
+}
---
+++ Assets/StoryStats.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ink.Runtime;
+using System.Text.RegularExpressions;
+
+public static class StoryStats
+{
+    public struct VariableStats
+    {
+        public double Minimum;
+        public double Maximum;
+        public double Mean;
+        public double Median;
+        public List<int> Modes;
+        public double StandardDeviation;
+    }
+
+    public const int Trials = 100;
+
+    public static VariableStats CalculateVariableStats(string storyJson, string variableName, int trials = Trials, string stoppingPoint = null)
+    {
+        var varStats = new VariableStats();
+
+        var story = new Story(storyJson);
+        var results = new List<int>();
+        var rng = new Random();
+
+        Regex stoppingPointRegex = string.IsNullOrEmpty(stoppingPoint) ? null : new Regex(stoppingPoint);
+
+        for (int currTrial = 0; currTrial < trials; currTrial++)
+        {
+            while (true)
+            {
+                if (stoppingPointRegex != null && stoppingPointRegex.IsMatch(story.currentText))
+                {
+                    break;
+                }
+
+                if (story.canContinue)
+                {
+                    story.Continue();
+                }
+                else if (story.currentChoices.Count > 0)
+                {
+                    story.ChooseChoiceIndex(rng.Next(story.currentChoices.Count));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            results.Add((int)story.variablesState[variableName]);
+            story.ResetState();
+        }
+
+        var doubleResults = results.Select(x => (double)x).OrderBy(x => x).ToList();
+
+        varStats.Minimum = doubleResults.First();
+        varStats.Maximum = doubleResults.Last();
+
+        varStats.Mean = doubleResults.Average();
+
+        int medianPoint = trials / 2;
+        if (trials % 2 == 0)
+        {
+            varStats.Median = (doubleResults[medianPoint] + doubleResults[medianPoint - 1]) / 2.0;
+        }
+        else
+        {
+            varStats.Median = doubleResults[medianPoint];
+        }
+
+        var counts = results.GroupBy(x => x);
+        var maxCount = counts.Select(g => g.Count()).Max();
+        varStats.Modes = counts.Where(g => g.Count() == maxCount).Select(g => g.Key).ToList();
+
+        varStats.StandardDeviation = Math.Sqrt(doubleResults.Select(x => Math.Pow(x - varStats.Mean, 2)).Sum() / trials);
+
+        return varStats;
+    }
+}
